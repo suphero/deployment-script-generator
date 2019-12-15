36899378489d5ddb92f8331ebd65463ce697629e
suphero <harunsokullu@gmail.com>
2019-12-15 15:29:19 +0300

    refactor

--- Code.js
+++ Code.js
@@ -17,7 +17,7 @@ function onOpen() {
   ui.createMenu('Download Scripts')
       .addItem('Preprod Scripts', 'downloadPreprodScripts')
       .addItem('Prod Scripts', 'downloadProdScripts')
-      .addItem('Complete Deployment', 'sprintDone')
+      .addItem('Complete Deployment', 'completeDeployment')
       .addToUi();
 }
 
@@ -25,7 +25,7 @@ function downloadPreprodScripts() {
   var t = HtmlService.createTemplateFromFile('download');
   t.data = getPreprodTable();
   t.environment = preprodEnv;
-  t.description = "Bu alanda sadece QA kolonu işaretlenmemişler gelmektedir. ";
+  t.description = "Bu alanda sadece QA kolonu işaretlenmemişler gelmektedir.";
   var html = t.evaluate();
   SpreadsheetApp.getUi().showModalDialog(html, 'Preprod Scripts');
 }
@@ -37,19 +37,19 @@ function getPreprodTable() {
 
 function getGroupedData(rawData) {
   var groupedData = [];
-  
+
   rawData.forEach(function (d) {
     var database = d[dbColumn];
     var type = d[typeColumn];
     var matched = false;
-    
+
     groupedData.forEach(function (g) {
       if (g[0] == database && g[1] == type) {
         g[2] += 1;
         matched = true;
       }
     });
-    
+
     if (!matched) {
       groupedData.push([database, type, 1, 'Button']);
     }
@@ -65,7 +65,7 @@ function getGroupedData(rawData) {
 function getPreprodData() {
   var rawData = getAllValues();
   var data = [];
-  
+
   rawData.forEach(function (d) {
     if (d[qaColumn] === false) { data.push(d); }
   });
@@ -77,7 +77,7 @@ function downloadProdScripts() {
   var t = HtmlService.createTemplateFromFile('download');
   t.data = getProdTable();
   t.environment = prodEnv;
-  t.description = "Bu alanda sadece Skip kolonu işaretlenmemişler bulunmaktadır. ";
+  t.description = "Bu alanda sadece Skip kolonu işaretlenmemişler bulunmaktadır.";
   var html = t.evaluate();
   SpreadsheetApp.getUi().showModalDialog(html, 'Prod Scripts');
 }
@@ -90,7 +90,7 @@ function getProdTable() {
 function getProdData() {
   var rawData = getAllValues();
   var data = [];
-  
+
   rawData.forEach(function (d) {
     if (d[skipColumn] === false) { data.push(d); }
   });
@@ -109,7 +109,6 @@ function getAllValues() {
     values.push(rawValues[i]);
   }
 
-  
   return values;
 }
 
@@ -120,7 +119,7 @@ function getFileId(filename, data) {
   var downloadURL = file.getDownloadUrl().replace("?e=download&gd=true","");
   return downloadURL;
 }
-  
+
 function getDownloadUrl(env, dbName, type) {
   var data;
   if (env == preprodEnv) {
@@ -135,30 +134,31 @@ function getDownloadUrl(env, dbName, type) {
   for (var i = 0; i < data.length; i++) {
     if (data[i][dbColumn] != dbName || data[i][typeColumn] != type) continue;
     script = script + spacer;
-    script = script + "\t--Owner : \t"+ data[i][personColumn] + "\n";
-    script = script + "\t--Task : \t"+ data[i][taskColumn] + "\n";
-    script = script + "\t--Type : \t"+ data[i][typeColumn] + "\n \n";
+    script = script + "\t--Owner: \t"+ data[i][personColumn] + "\n";
+    script = script + "\t--Task: \t"+ data[i][taskColumn] + "\n";
+    script = script + "\t--Type: \t"+ data[i][typeColumn] + "\n\n";
     script = script + data[i][scriptColumn] + "\n";
   }
-  
+
   var fileName = dbName + "_" + type + ".sql";
   var downloadId = getFileId(fileName, script);
   return downloadId;
 }
-function sprintDone(){
+
+function completeDeployment(){
   var done = HtmlService.createTemplateFromFile('transport');
   done.data = getPreprodTable();
   var html = done.evaluate();
   SpreadsheetApp.getUi().showModalDialog(html, 'HelloBro');
 }
+
 function transportData() {
   var transData = getAllValuesWithDate();
-  scriptToHistoryMapper (transData);
+  scriptToHistoryMapper(transData);
 }
 
 function getAllValuesWithDate() {
-  var data; 
-  data = getProdData();
+  var data = getProdData();
   var date = Utilities.formatDate(new Date(), "GMT+3", "dd/MM/yyyy")
   var endDate = date
 
@@ -170,14 +170,13 @@ function getAllValuesWithDate() {
 }
 
 function test(){
-  //getDownloadUrl(preprodEnv,"FleetUp","Schema");
   transportData();
 }
 
 function scriptToHistoryMapper(data) {
   var historyPersonColumn = 0;
   var historyTaskColumn = 1;
-  var historyDbColumb = 2;
+  var historyDbColumn = 2;
   var historyTypeColumn = 3;
   var historyScriptColumn = 4;
   var historyProdDateColumn = 5;
@@ -187,11 +186,10 @@ function scriptToHistoryMapper(data) {
   for (var i = 0; i < data.length; i++) {
     historyData[i][historyPersonColumn]= data[i][personColumn];
     historyData[i][historyTaskColumn]= data[i][taskColumn];
-    historyData[i][historyDbColumb]= data[i][dbColumn];
+    historyData[i][historyDbColumn]= data[i][dbColumn];
     historyData[i][historyTypeColumn]= data[i][typeColumn];
     historyData[i][historyScriptColumn]= data[i][scriptColumn];
     historyData[i][historyProdDateColumn]= data[i][dateColumn];
-    Logger.log( historyData);
+    Logger.log(historyData);
   }
-
 }
